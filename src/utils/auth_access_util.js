48af3c75e6f6fb9ee79c7e7dfa18a467bc1fc5b8
Luis <inside.developer888@gmail.com>
2025-11-11 12:03:40 +0800

    Testing

--- src/utils/auth_access_util.js
+++ src/utils/auth_access_util.js
@@ -98,7 +98,7 @@ function configJSAPIAccess(data, complete) {
 //处理用户免登逻辑
 export async function handleUserAuth(complete, organizationSlug = null) {
 
-    console.log("\n----------[接入方网页免登处理 BEGIN]----------")
+    console.log("\n----------[接入方网页免登处理 BEGIN11111]----------")
     
     // Get organization slug from parameter or localStorage
     const orgSlug = organizationSlug || localStorage.getItem(ORGANIZATION_SLUG_KEY) || null;
